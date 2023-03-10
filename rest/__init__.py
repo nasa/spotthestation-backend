d328943e9b81ab6b0ae386466fa4ceda4c17a2d8
Andrew Drach <andrew@solwey.com>
2023-03-10 13:50:54 -0500

    chore: add health check; update requirements

--- rest/__init__.py
+++ rest/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
 from .routes import tracking
 
-app = Flask(__name__)
+health_status = True
 
+app = Flask(__name__)
 app.register_blueprint(tracking.bp)
+
+@app.route('/health')
+def health():
+    if health_status:
+        resp = jsonify(health="healthy")
+        resp.status_code = 200
+    else:
+        resp = jsonify(health="unhealthy")
+        resp.status_code = 500
+
+    return resp
