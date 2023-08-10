042fb6686277b36086248643e17f2999394a18d0
Andrew Drach <andrew@solwey.com>
2023-08-10 12:56:08 -0500

    feat: replace linear interpolation with Catmull-Rom spline

--- rest/tasks.py
+++ rest/tasks.py
@@ -5,6 +5,7 @@ from redis import Redis
 import os
 import pickle
 import numpy
+from splines import CatmullRom
 
 from skyfield.constants import AU_M
 from skyfield.positionlib import ICRS
@@ -12,7 +13,7 @@ from skyfield.api import load
 from skyfield.toposlib import Topos
 
 from skyfield.api import utc
-from datetime import datetime
+from datetime import datetime, timedelta
 from .services.helpers import (
     format_epoch, GCRF_to_ITRF, earthPositions, Topos_xyz, linear_interpolation, download
 )
@@ -34,26 +35,50 @@ def get_sat_data():
     earth = eph['earth']
     ts = load.timescale()
 
-    sat = []
+    spline_points = []
     for epoch in epoches:
-        date = datetime.strptime(epoch['date'], "%Y-%m-%dT%H:%M:%S.%f").replace(tzinfo=utc)
-        r, v = GCRF_to_ITRF(epoch['location'], epoch['velocity'], date, earth_positions)
-        t = Topos_xyz(r[0], r[1], r[2])
+        spline_points.append((epoch['location'][0], epoch['location'][1], epoch['location'][2]))
 
-        epos = earth.at(ts.from_datetime(date)).position.km
-        pos = (earth + Topos(t.latitude.degrees, t.longitude.degrees)).at(ts.from_datetime(date)).position.km
-        er = numpy.sqrt(((pos - epos) ** 2).sum())
+    curve = CatmullRom(spline_points)
+    sat = []
+    interpolated_sat = []
 
-        sat.append({
-            'date': date,
-            'location': r,
-            'velocity': v,
-            'altitude': ICRS((r[0] * 1000 / AU_M, r[1] * 1000 / AU_M,
-                             r[2] * 1000 / AU_M)).distance().km - er
-        })
+    for i in range(len(epoches) - 1):
+        start = datetime.strptime(epoches[i]['date'], "%Y-%m-%dT%H:%M:%S.%f").replace(tzinfo=utc)
+        end = datetime.strptime(epoches[i + 1]['date'], "%Y-%m-%dT%H:%M:%S.%f").replace(tzinfo=utc)
 
-    sat_data = linear_interpolation(sat, 16)
-    redis.set('sat_data', pickle.dumps(sat_data))
+        steps = max(1, int((end - start).total_seconds() // 15))
+
+        for j in range(steps):
+            t0 = i
+            t1 = i + 1
+            t = t0 + (t1 - t0) * (j / steps)
+            pt = curve.evaluate(t)
+            date = (start + timedelta(seconds=j*15))
+
+            r, v = GCRF_to_ITRF(pt, epoches[i]['velocity'], date, earth_positions)
+            t = Topos_xyz(r[0], r[1], r[2])
+
+            epos = earth.at(ts.from_datetime(date)).position.km
+            pos = (earth + Topos(t.latitude.degrees, t.longitude.degrees)).at(ts.from_datetime(date)).position.km
+            er = numpy.sqrt(((pos - epos) ** 2).sum())
+
+            if j == 0:
+                sat.append({
+                    'date': date,
+                    'location': r,
+                    'altitude': ICRS((pt[0] * 1000 / AU_M, pt[1] * 1000 / AU_M,
+                                      pt[2] * 1000 / AU_M)).distance().km - er
+                })
+
+            interpolated_sat.append({
+                'date': date,
+                'location': r,
+                'altitude': ICRS((pt[0] * 1000 / AU_M, pt[1] * 1000 / AU_M,
+                                  pt[2] * 1000 / AU_M)).distance().km - er
+            })
+
+    redis.set('sat_data', pickle.dumps(interpolated_sat))
     redis.set('sat_data_not_interpolated', pickle.dumps(sat))
     redis.set('sat_data_updated_at', datetime.now(tz=utc).isoformat())
-    return sat_data
+    return interpolated_sat
